65f4dca783a8691e578eff10a75bcc0934c42166
Kate McGee <mcgeehee@gmail.com>
2020-02-24 21:36:59 -0700

    finished project without stretch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
+  <div>
     <Navigation />
+    
+  </div>
+  <div>
+  <Route exact path='/' component={Home}/>
+  <Route path="/about">
+    <About />
+  </Route>
+  <Route path='/contact' component={Contact}/>
+  </div>
   </div>
 );
 
+//-------------------------------------------------------------
+//Notes
+//-------------------------------------------------------------
+
+//path is a prop
+//component is a prop
+//you can name path anything you want to, but it's good to be semantic
+//exact is a prop and means only render when the slash exactly matches
+//if you don't have anything after a prop it defaults to true so we don't have to have exact={true} it's redundant
+
+
+
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <BrowserRouter> */}
+//         <Link to='/about'>About</Link>
+//         <Link to='/'>Home</Link>
+//         <Link to='/contact'>Contact</Link>
+//         <Link to='/blog'>Blog</Link>
+
+//         <Route path='/blog' component={Blog}/>
+//         <Route exact path='/' component={Home}/>
+//         <Route path='/about' component={About} />
+//         <Route path='/contact' component={Contact}/>
+//         {/* </BrowserRouter> */}
+//         <Route path='/blog' render={(props) => {
+//           debugger
+//           return <h1>The Blog</h1>
+//         }}/>
+//       </header>
+//     </div>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router} from "react-router-dom"; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//we use Router as our base component that will wrap our entire app. We'll do this by wrapping
+//our <App /> component that we pass to ReactDOM.render
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
